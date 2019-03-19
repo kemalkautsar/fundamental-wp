efb00aeb8ece22e94ffbbbfe4c132a4d0615a3eb
kemalkautsar <kemal.kautsar@gmail.com>
2019-03-19 14:34:40 +0700

    fix: comment form styling

--- comments.php
+++ comments.php
@@ -5,7 +5,8 @@
 	<?php } else { ?>
 		<h3 class="comment-post-title">There are no comments yet. Start the conversation here!</h3>
 	<?php } ?>
-
+	
+	<div class="comment-post-conversation">
 	<?php 
 		$args = array(
 			'walker'            => null,
@@ -22,18 +23,24 @@
 			'reverse_children'  => '',
 			'format'            => 'html5', // or 'xhtml' if no 'HTML5' theme support
 			'short_ping'        => false,   // @since 3.6
-				'echo'              => true     // boolean, default is true
+			'echo'              => true     // boolean, default is true
 		);
 
 		wp_list_comments($args, $comments);
+		?>
+		</div>
 
+		<?php
 		$comments_args = array(
 			// change the title of send button 
-			'label_submit'=>'Post Comment',
+			'label_submit'			=>'Post Comment',
 			// remove "Text or HTML to be displayed after the set of comment fields"
-			'comment_notes_after' => '',
+			'comment_notes_after' 	=> '',
 			// redefine your own textarea (the comment body)
-			'comment_field' => '<p class="comment-form-comment"><textarea id="comment" name="comment" aria-required="true" placeholder="Comment"></textarea></p>',
+			'comment_field' 		=> '<p class="comment-form-comment"><textarea id="comment" name="comment" aria-required="true" placeholder="Comment"></textarea></p>',
+			'title_reply'			=> 'Leave a comment',
+			'title_reply_before'	=> '<h4 id="reply-title" class="comment-reply-title">',
+			'title_reply_after'		=> '</h4>'
 		);
 		
 		comment_form($comments_args);
